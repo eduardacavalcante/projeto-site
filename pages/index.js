f0a7160c023798fccf68f043c19a79a74844573d
Eduarda Cavalcante <x20146531@student.ncirl.ie>
2022-02-28 22:18:03 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import { useState } from  'react';
 
 function Home(){
-  return <div>Hooome</div>
+  return <div>Passando so pra dizer que eu te amo, ta?</div>
+  
 }
+
+function Lov(){
+   
+}
+
+alert("Oi, amor!");
 
 
 export default Home
